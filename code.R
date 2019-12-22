9b9af1c66f3c12a8b468604d8981be9821f44b2e
Hutch Hahm <hutch@naver.com>
2019-12-22 21:40:17 +0900

    Update code.R

--- code.R
+++ code.R
@@ -4,5 +4,6 @@ library(pdftools)
 term1 <- pdf_text("/Users/hutch/OneDrive - 중앙대학교/다운로드/usd_fam_3(20190601).pdf")
 cat(txt[1])
 list = grep(pattern = "보험금의 지급", x = term1)
-cat(term1[list[2]])
-
+term_guarantee = term1[list[2]]
+term_clean = capture.output(cat(term_guarantee))
+term_clean[1:5]
